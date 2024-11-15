59aaaebc20672c495457a9db19beae9ce8bbffb5
Diyorbek1213-sys <birorbirorkim@gmail.com>
2024-11-15 20:16:26 +0500

    3 oy 11 uy

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {},
+
+      fontFamily: {
+        'Raleway': ['Raleway', 'sans-serif'],
+        'Inter': ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
